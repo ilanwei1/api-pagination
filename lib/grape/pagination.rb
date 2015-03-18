d5164c923dd148cc85bec46ad4eba837c4b04c06
ilan weissberg <ilaneitor@gmail.com>
2015-03-18 10:25:11 +0200

    remove route_setting missing method

--- lib/grape/pagination.rb
+++ lib/grape/pagination.rb
@@ -3,10 +3,10 @@ module Grape
     def self.included(base)
       Grape::Endpoint.class_eval do
         def paginate(collection)
-          per_page = params[:per_page] || route_setting(:per_page)
+          per_page = params[:per_page]
           options = {
             :page     => params[:page],
-            :per_page => [per_page, route_setting(:max_per_page)].compact.min
+            :per_page => [per_page].compact.min
           }
           collection = ApiPagination.paginate(collection, options)
 
@@ -17,13 +17,13 @@ module Grape
           pages.each do |k, v|
             old_params = Rack::Utils.parse_query(request.query_string)
             new_params = old_params.merge('page' => v)
-            links << %(<#{url}?#{new_params.to_param}>; rel="#{k}")
+            links << %(#{url}?#{new_params.to_param};#{k})
           end
 
           total_header    = ApiPagination.config.total_header
           per_page_header = ApiPagination.config.per_page_header
 
-          header 'Link',          links.join(', ') unless links.empty?
+          header 'Link',          links.join(',') unless links.empty?
           header total_header,    ApiPagination.total_from(collection)
           header per_page_header, options[:per_page].to_s
 
@@ -33,8 +33,6 @@ module Grape
 
       base.class_eval do
         def self.paginate(options = {})
-          route_setting :per_page, (options[:per_page] || 25)
-          route_setting :max_per_page, options[:max_per_page]
           params do
             optional :page,     :type => Integer, :default => 1,
                                 :desc => 'Page of results to fetch.'
